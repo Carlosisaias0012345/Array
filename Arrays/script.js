bcbe3b3c39d5b8f349e3ee6ebe28e765a0a15458
Carlosisaias0012345 <109426872+Carlosisaias0012345@users.noreply.github.com>
2023-03-27 12:36:05 -0300

    Add files via upload

---
+++ Arrays/script.js
@@ -0,0 +1,9 @@
+let arrayInValid = ['Adicionar', 'Ordenar',10,true ];
+let arrayValid = ['Cadeira', 'Impressora', 'Garfo'];
+console.log(arrayValid[0]);
+
+var objetos = Array('Cadeira', 'Impressora', 'Garfo');
+
+const input = document.getElementById('input');
+const adicionar = document.getElementById('adicionar');
+const ordenar = document.getElementById('ordenbar');
